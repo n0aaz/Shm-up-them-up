f26783be61a2c60341bd069c4e9bf66725887c0f
thb003 <theobredoux@gmail.com>
2016-02-28 20:43:43 +0100

    Optimisation de la fonction
    Il suffit, lorsqu’on fait appel à la fonction d’entrer les variables
    nécessaires à la mise en forme du texte. Cela permet de l’utiliser dans
    encore plus de cas. Cette fonction "passe partout" sera la base de
    toutes les fonctions d’affichage de texte aussi bien pour les messages,
    les informations du joueur ou l’affichage des menus.

--- Textes.py
+++ Textes.py
@@ -11,19 +11,21 @@ white = (255,255,255)
 red = (255,0,0)
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 
-def texte_police(texte, police):
-    forme_texte = police.render(texte, True, white)
+def texte_police(texte, police,couleur):
+    forme_texte = police.render(texte, True, couleur) #Mise en forme du texte avec la couleur
     return forme_texte, forme_texte.get_rect()
 
-
-def message_display(text):
-    taille_texte = pygame.font.Font('ressources/polices/minecraft.ttf',115)
-    forme_texte, position_texte = texte_police(text, taille_texte)
-    position_texte.center = ((display_width/2),(display_height/2))
-    gameDisplay.blit(forme_texte, position_texte)
+ """Fonction permettant d'afficher un message, a pour entrées le texte,
+    la position relative en x et y, la taille et la couleur du texte soit en préréglage, soit en RGB (xRx,xGx,xBx). Elle
+    s'occupe de faire appel aux autres fonctions de mise en forme de texte, gérer la police et la taille puis afficher le texte"""
+def message_display(text,px,py,taille,couleur):
+    taille_texte = pygame.font.Font('ressources/polices/minecraft.ttf',taille) #Mise en forme de la police avec taille
+    forme_texte, position_texte = texte_police(text, taille_texte,couleur) #appel a la fonction texte_police
+    position_texte.center = ((px),(py)) #position du texte
+    gameDisplay.blit(forme_texte, position_texte) #affichage du texte
 
 
 while True :
-    message_display("Test")
+    message_display("Test",500,200,115,white)
     pygame.display.update()
 
