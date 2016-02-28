95cfeb57fd08e99489299dce832658f924bb38b0
thb003 <theobredoux@gmail.com>
2016-02-28 19:44:49 +0100

    Affichage d'in texte
    Fonction a ajouter au programme principal

---
+++ Textes.py
@@ -0,0 +1,29 @@
+import pygame
+
+
+pygame.init()
+
+display_width = 800
+display_height = 600
+
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+gameDisplay = pygame.display.set_mode((display_width,display_height))
+
+def texte_police(texte, police):
+    forme_texte = police.render(texte, True, white)
+    return forme_texte, forme_texte.get_rect()
+
+
+def message_display(text):
+    taille_texte = pygame.font.Font('ressources/polices/minecraft.ttf',115)
+    forme_texte, position_texte = texte_police(text, taille_texte)
+    position_texte.center = ((display_width/2),(display_height/2))
+    gameDisplay.blit(forme_texte, position_texte)
+
+
+while 1 :
+    message_display("crash")
+    pygame.display.update()
+
