07aa88143d669a027be332c60138b8ff406610b0
thb003 <theobredoux@gmail.com>
2016-02-28 21:03:27 +0100

    Réduction des entrées
    Parce que c’est mieux

--- Textes.py
+++ Textes.py
@@ -15,10 +15,10 @@ def texte_police(texte, police,couleur):
     forme_texte = police.render(texte, True, couleur) #Mise en forme du texte avec la couleur
     return forme_texte, forme_texte.get_rect()
 
-def message_display(text,px,py,taille,couleur):
+def message_display(text,po,taille,couleur):
     taille_texte = pygame.font.Font('ressources/polices/minecraft.ttf',taille) #Mise en forme de la police avec taille
     forme_texte, position_texte = texte_police(text, taille_texte,couleur) #appel a la fonction texte_police
-    position_texte.center = ((px),(py)) #position du texte
+    position_texte.center = (po) #position du texte
     gameDisplay.blit(forme_texte, position_texte) #affichage du texte
 
 def surlignage(etat):
@@ -31,5 +31,5 @@ def surlignage(etat):
 
 
 while True :
-    message_display("Test",500,200,115,surlignage(False))
+    message_display("Test",(500,200),115,surlignage(False))
     pygame.display.update()
