b7f2fe9bb6f4252eb9609a8999e702ef2969abb8
thb003 <theobredoux@gmail.com>
2016-02-28 21:02:17 +0100

    Améliorations de la fonction
    Ajout d’un surlignage pour la couleur blanche pour avoir un effet sur
    le texte ultérieurement

--- Textes.py
+++ Textes.py
@@ -15,17 +15,21 @@ def texte_police(texte, police,couleur):
     forme_texte = police.render(texte, True, couleur) #Mise en forme du texte avec la couleur
     return forme_texte, forme_texte.get_rect()
 
- """Fonction permettant d'afficher un message, a pour entrées le texte,
-    la position relative en x et y, la taille et la couleur du texte soit en préréglage, soit en RGB (xRx,xGx,xBx). Elle
-    s'occupe de faire appel aux autres fonctions de mise en forme de texte, gérer la police et la taille puis afficher le texte"""
 def message_display(text,px,py,taille,couleur):
     taille_texte = pygame.font.Font('ressources/polices/minecraft.ttf',taille) #Mise en forme de la police avec taille
     forme_texte, position_texte = texte_police(text, taille_texte,couleur) #appel a la fonction texte_police
     position_texte.center = ((px),(py)) #position du texte
     gameDisplay.blit(forme_texte, position_texte) #affichage du texte
 
+def surlignage(etat):
+    if etat == True :
+        pig= (255,255,255)
+    else :
+        pig=(100, 100,100)
+
+    return pig
+
 
 while True :
-    message_display("Test",500,200,115,white)
+    message_display("Test",500,200,115,surlignage(False))
     pygame.display.update()
-
