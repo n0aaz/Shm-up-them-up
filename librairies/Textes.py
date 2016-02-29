9f80b80e933640bb52b579f1ded896c38192ae62
thb003 <theobredoux@gmail.com>
2016-02-29 09:45:08 +0100

    Transformation en Classe

--- librairies/Textes.py
+++ librairies/Textes.py
@@ -1,20 +1,26 @@
 import pygame
+class  textes(pygame.sprite.Sprite) :
+    def __init__(self):
+        self.texte = "texte"
+        self.taille = 115
+        self.couleur = surlignage(True)
+        self.image = message_display(texte,taille,couleur)
 
 
-def texte_police(texte, police,couleur):
-    forme_texte = police.render(texte, True, couleur) #Mise en forme du texte avec la couleur
-    return forme_texte, forme_texte.get_rect()
+    def texte_police(texte, police,couleur):
+        forme_texte = police.render(texte, True, couleur) #Mise en forme du texte avec la couleur
+        return forme_texte,
 
-def message_display(text,po,taille,couleur,fenetre):
-    taille_texte = pygame.font.Font('ressources/polices/minecraft.ttf',taille) #Mise en forme de la police avec taille
-    forme_texte, position_texte = texte_police(text, taille_texte,couleur) #appel a la fonction texte_police
-    position_texte.center = (po) #position du texte
-    fenetre.blit(forme_texte, position_texte) #affichage du texte
+    def message_display(text,taille,couleur):
+        taille_texte = pygame.font.Font('ressources/polices/minecraft.ttf',taille) #Mise en forme de la police avec taille
+        forme_texte= texte_police(text, taille_texte,couleur) #appel a la fonction texte_police
+        return forme_texte
 
-def surlignage(etat):
-    if etat == True :
-        pig= (255,255,255)
-    else :
-        pig=(100, 100,100)
 
-    return pig
+    def surlignage(etat):
+        if etat == True :
+            pig= (255,255,255)
+        else :
+            pig=(100, 100,100)
+
+        return pig
