fa544f14d5eb349ca8775f466b8ff18422661cb8
thb003 <theobredoux@gmail.com>
2016-04-04 09:49:24 +0200

    #Alicia Fonction pour trier une liste
    Fonction d'Alicia pour trier une liste dans l'ordre croissant

---
+++ fctcroissant.py
@@ -0,0 +1,34 @@
+﻿# Créé par aduguaitschupp, le 21/03/2016 en Python
+
+
+x = [122,38,45,11,58,68,12,1,5,35,48]
+tri = []
+
+
+
+
+
+def minimum (x) :
+    lg  = len(x)
+    min = x[0]
+    for i in range (lg):
+        if x[i]<min :
+            min = x[i]
+    return (min)
+
+print('le minimum dans la liste est : ',minimum(x))
+
+for i in x :
+    tri.append(minimum(x))
+    print (tri)
+    min = minimum(x)
+    x.remove (min)
+    print(x)
+
+
+
+
+
+
+
+
